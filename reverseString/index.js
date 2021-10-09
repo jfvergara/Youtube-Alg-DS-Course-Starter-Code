2dbb4dd70da4a86ca650799829d40d3aa291921c
Francisco Vergara Espinoza <neo_ca@live.com.mx>
2021-10-09 17:26:02 -0500

    reverseString algorithm completed :)

--- reverseString/index.js
+++ reverseString/index.js
@@ -4,7 +4,34 @@
 //   reverse('abcd') === 'dcba'
 //   reverse('Hello!') === '!olleH'
 
-function reverse(str) {}
+function reverse(str) {
+  //Solution 01
+  //   const splited = str.split("");
+  //   splited.reverse();
+  //   const reversedString = splited.join("");
+  //   return reversedString;
+  //   return str.split("").reverse().join("");
+
+  //Solution 02
+  // return str.split("").reverse().join("");
+
+  //Solution 03
+  //   const array = str.split("");
+  //   var arrayRversed = [];
+  //   for (let index = array.length - 1; index >= 0; index--) {
+  //     arrayRversed.push(array[index]);
+  //   }
+  //   return arrayRversed.join("");
+
+  //Solution 03
+
+  let res = "";
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    res = char + res;
+  }
+  return res;
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -29,14 +56,14 @@ function reverse(str) {}
 //                          ______ ______ ______ ______ ______
 //                         |______|______|______|______|______|
 
-mocha.setup('bdd');
+mocha.setup("bdd");
 const { assert } = chai;
 
-describe('String Reversal', () => {
-	it('reverse() correctly reverses string', () => {
-		assert.equal(reverse('ffaa'), 'aaff');
-		assert.equal(reverse('  aaff'), 'ffaa  ');
-	});
+describe("String Reversal", () => {
+  it("reverse() correctly reverses string", () => {
+    assert.equal(reverse("ffaa"), "aaff");
+    assert.equal(reverse("  aaff"), "ffaa  ");
+  });
 });
 
 mocha.run();
